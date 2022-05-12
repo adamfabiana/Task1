e6343441724fd9b1208abecb532e307f0bfd04dc
Miruna <miruna_munteanu2009@yahoo.com>
2022-05-12 19:30:30 +0300

    commit nou

--- src/github/clasa.java
+++ src/github/clasa.java
@@ -5,11 +5,13 @@ public class clasa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello!");
-		int a = 6;
-		System.out.println("a = " + a);
-		int b = 8;
-		System.out.println("b = " + b);
+		String s = "Buna buna vrei sa facem schimb de guma?";
+
+        System.out.println("String s = " + s);
+
+        String s1 = new String("hei!");
+ 
+        System.out.println("String s1 = " + s1);
 	}
 
 }
