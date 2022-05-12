0b4748f07b9d60de550b74b4c1a500c68277a9fa
adamf <adamf@LAPTOP-9036782A>
2022-05-12 18:49:41 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+
+# Hello World Project
+## A git demonstration
+
+Adam Fabiana
+
+fabiana-stefania.adam@student.tuiasi.ro
+
+This repository is hosted at:
+
+https://github.com/adamfabiana/Task1.git
---
+++ src/github/clasa.java
@@ -0,0 +1,13 @@
+package github;
+
+public class clasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello!");
+		int a=6;
+		System.out.println("a=" + a);
+	}
+
+}
